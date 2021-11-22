9727eba0ba1834db44d76f5df55c923b7db01768
wcocuzza <wallace.cocuzza@gmail.com>
2021-11-22 20:51:21 -0300

    Desenvolvido o middleware, Criação do usuario e listar todos

--- src/index.js
+++ src/index.js
@@ -1,29 +1,48 @@
 const express = require('express');
 const cors = require('cors');
-
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const {username} = request.headers;
+  const user = users.find(user => user.username == username);
+  if(!user) {
+    return response.status(400).send({error: "Username not exists"});
+  }
+  request.username = user;
+  return next();
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  //Create user
+  const {name, username} = request.body;
+  const userAlreadyExists = users.some((user) => user.username === username);
+  if(userAlreadyExists) {
+    return response.status(400).json({error: "Username already exists"});
+  }
+  users.push({
+    id: uuidv4(),
+    name,
+    username,
+    todos: []
+  });
+  return response.status(201).send();
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const {username} = request;
+  console.log(username)
+  return response.json(username.todos);
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
